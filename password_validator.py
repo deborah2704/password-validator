e34c98bdc427e5a788dc258ff58eed53bc2c74a5
deborah2704 <89034055+deborah2704@users.noreply.github.com>
2022-07-13 23:22:58 +0300

    Fixing functions

--- password_validator.py
+++ password_validator.py
@@ -1,29 +1,50 @@
 import sys
 
+# Get error messages according to what's missing
+def get_message(has_min_len, has_letter, has_number, has_upper, has_lower):
+    message = ''
+    if not has_min_len:
+        message += "Your password should be at least 10 characters\n"
+    if not has_letter:
+        message += "The password should contains at least one alphabetical characters\n"
+    if not has_number:
+        message += "Your password should contains at least one numerical characters\n"
+    if not has_upper:
+        message += "Your password should contains at least one capital letter\n"
+    if not has_lower:
+        message += "Your password should contains at least one lower letter"
+    return message
+
+# Validate the password, prints the success message or the error message and exit with code (0 -> success, 1 -> error)
 def validate(p):
     code = 0
-    message = 'The password is valid'
-    if(len(p) <= 10):
-        message = "Your password should be at least 10 characters"
+    has_min_len = False
+    has_number = False
+    has_letter = False
+    has_upper = False
+    has_lower = False
+    if(len(p) >= 10):
+        has_min_len = True
+    for c in p:
+        if c.isalpha():
+            has_letter = True
+        if c.isdigit():
+            has_number = True
+        if c.isupper():
+            has_upper = True
+        if c.islower():
+            has_lower = True
+        
+    # If one of the condition is false so the password is incorrect
+    if (not (has_min_len and has_letter and has_number and has_upper and has_lower)):
         code = 1
-    if p.isalpha():
-        message = "Your password should contains alphabetical characters"
-        code = 1
-    if p.isdigit():
-        message = "Your password should contains numerical characters"
-        code = 1
-    if p.isupper():
-        message = "Your password should contains at least one capital letter"
-        code = 1
-    if p.islower():
-        message = "Your password should contains at least one lower letter"
-        code = 1
-    # Color code RED if there is an error
-    if code == 1:
+        message = get_message(has_min_len, has_letter, has_number, has_upper, has_lower)
         color = '\033[91m'
-    # Color code GREEN if the password fill the requirement
     else:
+        code = 0
+        message = 'The password is valid'
         color = '\033[92m'
+
     print(color + message + '\033[0m')
     sys.exit(code)
 
